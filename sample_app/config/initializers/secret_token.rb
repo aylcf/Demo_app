6aadbd9d79be6f81c56aac1456b4f9892020cab6
aylcf <aylcf@163.com>
2014-04-20 13:03:52 +0800

    Signed-off-by: aylcf <aylcf@163.com>

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '11f82485eab8a815268024f4280751824df6e95c9ce237aa507e6c84e41de69fb2530e0cdc1abbc052e95f09ff9a87c3501cf1da2ef362f7e2dbce98a9c8df7a'
